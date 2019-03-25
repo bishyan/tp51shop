f1a7b0ba4542119a8b39e05c3db4f37f81119533
bishyan <maijie5201314@163.com>
2019-03-25 22:32:16 +0800

    ...

---
+++ application/admin/model/Admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/25
+ * Time: 21:13
+ */
+
+namespace app\admin\model;
+
+
+class Admin
+{
+    protected $autoWriteTimestamp = true;
+}
