8e59a35d2b0f64e4347e219b3aa2bc57457d27e4
bishyan <maijie5201314@163.com>
2019-03-25 11:34:57 +0800

    Initial commit

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/25
+ * Time: 11:19
+ */
+
+namespace app\admin\controller;
+use think\Db;
+
+class Admin extends Base
+{
+    public function login()
+    {
+        return $this->fetch();
+    }
+}
---
+++ application/admin/controller/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/25
+ * Time: 11:21
+ */
+
+namespace app\admin\controller;
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
